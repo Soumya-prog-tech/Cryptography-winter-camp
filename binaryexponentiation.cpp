d10a0b81d386b782d9a0b04a796860ad17a97380
Soumya Jhunjhunwala <soumyajhunjhunwala@gmail.com>
2024-12-03 08:55:31 +0530

    binaryexpo iterative method

--- binaryexponentiation.cpp
+++ binaryexponentiation.cpp
@@ -21,9 +21,23 @@ int binexporecurr(int a,int b)
      }
      
 }
+//Iterative method
+int binexpoiterate(int a,int b)
+{
+    int ans = 1;
+    
+    while(b)
+    {
+        if(b&1) ans = (ans*1ll*a)%M;
+        a = (a*1ll*a)%M;
+        b>>=1;
+    }
+    return ans;
+}
 int main() 
 {
     int a,b;
     cin>>a>>b;
-    cout<<binexporecurr(a,b);
+    cout<<binexporecurr(a,b)<<endl<<binexpoiterate(a,b);
 }
+
